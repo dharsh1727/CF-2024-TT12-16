03a8e8b7c49028d75f56726bcc9ed438f732544f
dharsh1727 <dharsh1727s@gmail.com>
2025-09-09 00:41:18 +0530

    Update wptr_full.v

--- src/wptr_full.v
+++ src/wptr_full.v
@@ -1,4 +1,3 @@
-
 module wptr_full #(parameter ADDR_WIDTH = 3)(
     input wclk, rst_n, winc,
     input [ADDR_WIDTH:0] rptr_sync,
@@ -13,22 +12,16 @@ module wptr_full #(parameter ADDR_WIDTH = 3)(
     assign wgray_next = (wbin_next >> 1) ^ wbin_next;
 
     always @(posedge wclk or negedge rst_n) begin
-        if (!rst_n)
-            wbin <= 0;
-        else
-            wbin <= wbin_next;
-    end
-
-
-    always @(posedge wclk or negedge rst_n) begin
-        if (!rst_n)
-            full <= 1'b0;
+        if (!rst_n) begin
+            wbin  <= 0;
+            full  <= 1'b0;
             wptr  <= 0;
             waddr <= 0;
-        else
-            full <= (wgray_next == {~rptr_sync[ADDR_WIDTH:ADDR_WIDTH-1], rptr_sync[ADDR_WIDTH-2:0]});
+        end else begin
+            wbin  <= wbin_next;
+            full  <= (wgray_next == {~rptr_sync[ADDR_WIDTH:ADDR_WIDTH-1], rptr_sync[ADDR_WIDTH-2:0]});
             wptr  <= wgray_next;
-            waddr <= wbin[ADDR_WIDTH-1:0];
+            waddr <= wbin_next[ADDR_WIDTH-1:0];
+        end
     end
 endmodule
-
