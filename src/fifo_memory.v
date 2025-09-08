b6a10b20488cd44eed0238c2af71b9d0a12c411a
dharsh1727 <dharsh1727s@gmail.com>
2025-09-09 00:43:31 +0530

    Update fifo_memory.v

--- src/fifo_memory.v
+++ src/fifo_memory.v
@@ -1,5 +1,3 @@
-
-
 module fifo_memory #(parameter DATA_WIDTH = 4, ADDR_WIDTH = 3)(
     input wire wclk, rclk,
     input wire [ADDR_WIDTH-1:0] waddr, raddr,
@@ -8,15 +6,24 @@ module fifo_memory #(parameter DATA_WIDTH = 4, ADDR_WIDTH = 3)(
     output reg [DATA_WIDTH-1:0] rdata
 );
     reg [3:0] mem [(2**ADDR_WIDTH)-1:0];
-    always @(posedge wclk) begin
-        if (wen) begin
+
+    // Write with reset: option to clear memory on reset
+    integer i;
+    always @(posedge wclk or negedge ren) begin
+        if (!ren) begin
+            for (i = 0; i < (2**ADDR_WIDTH); i = i + 1)
+                mem[i] <= 0;
+        end else if (wen) begin
             mem[waddr] <= wdata;
         end
     end
 
-    always @(posedge rclk) begin
-        if (ren) begin
-          rdata <= mem[raddr];
+    // Read with reset: clear rdata on reset
+    always @(posedge rclk or negedge ren) begin
+        if (!ren) begin
+            rdata <= 0;
+        end else if (ren) begin
+            rdata <= mem[raddr];
         end 
     end
 endmodule
