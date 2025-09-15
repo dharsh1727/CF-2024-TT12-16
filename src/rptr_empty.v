17fd12d6fa72c23f7926f23d686151fd6896db00
dharsh1727 <dharsh1727s@gmail.com>
2025-09-15 17:31:53 +0530

    Update rptr_empty.v

--- src/rptr_empty.v
+++ src/rptr_empty.v
@@ -3,11 +3,12 @@ module rptr_empty #(parameter ADDR_WIDTH = 3)(
     input [ADDR_WIDTH:0] wptr_sync,
     output reg empty,
     output reg [ADDR_WIDTH:0] rptr,
-    output reg [ADDR_WIDTH-1:0] raddr
+    output  [ADDR_WIDTH-1:0] raddr
 );
     reg [ADDR_WIDTH:0] rbin;
     wire [ADDR_WIDTH:0] rbin_next, rgray_next;
 
+    assign raddr = rbin_next[ADDR_WIDTH-1:0];
     assign rbin_next = rbin + ((rinc & ~empty) ? 1 : 0);
     assign rgray_next = (rbin_next >> 1) ^ rbin_next;
  
@@ -16,12 +17,11 @@ module rptr_empty #(parameter ADDR_WIDTH = 3)(
             rbin  <= 0;
             empty <= 1'b1;
             rptr  <= 0;
-            raddr <= 0;
+            
         end else begin
             rbin  <= rbin_next;
             empty <= (rgray_next == wptr_sync);
             rptr  <= rgray_next;
-            raddr <= rbin_next[ADDR_WIDTH-1:0];
         end
     end
 endmodule
